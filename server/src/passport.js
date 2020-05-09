a014d98ab2cb33a9418be42a6d12d2cc425f3df6
Cesar <cesar.kina@gmail.com>
2020-05-08 22:20:18 -0500

    fix cookie

--- server/src/passport.js
+++ server/src/passport.js
@@ -27,10 +27,7 @@ module.exports = (app) => {
       saveUninitialized: false,
       cookie: {
         secure: !isDevelopment,
-        cookie: {
-          secure: !isDevelopment,
-          expires: new Date(Date.now() + 30 * 86400 * 1000),
-        },
+        expires: new Date(Date.now() + 30 * 86400 * 1000),
       },
       store: new redisStore({
         client: redisClient,
