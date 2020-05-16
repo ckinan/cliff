6a2c05d37c65e093701f20680c74161366b39c20
Cesar <cesar.kina@gmail.com>
2020-05-16 17:53:21 -0500

    Client: Introducing TypeScript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,27 +3,30 @@ import moment from 'moment';
 import { Switch, Route, useHistory } from 'react-router-dom';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const history = useHistory();
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_SERVER_URL}/api/me`, {
+    const headers: HeadersInit = new Headers();
+    headers.set('Access-Control-Allow-Credentials', 'true');
+
+    const params: RequestInit = {
       method: 'GET',
       credentials: 'include',
-      headers: {
-        'Access-Control-Allow-Credentials': true,
-      },
-    })
-      .then(function (response) {
+      headers: headers,
+    };
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/api/me`, params)
+      .then(response => {
         return response.json();
       })
-      .then(function (json) {
+      .then(json => {
         setIsAuthenticated(json.isAuthenticated);
       });
   }, []);
 
   useEffect(() => {
-    if (isAuthenticated === true) {
+    if (isAuthenticated) {
       history.push(`/app`);
     } else {
       history.push(`/`);
@@ -59,45 +62,58 @@ function Public() {
   return <div>Hi! I am public!</div>;
 }
 
-function Header({ isAuthenticated, setIsAuthenticated }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+interface IAuthProps {
+  isAuthenticated: boolean;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
+}
 
-  const handleLogout = async (e) => {
+const Header: React.FC<IAuthProps> = ({ isAuthenticated, setIsAuthenticated }) => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogout = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    fetch(`${process.env.REACT_APP_SERVER_URL}/api/logout`, {
+
+    const headers: HeadersInit = new Headers();
+    headers.set('Access-Control-Allow-Credentials', 'true');
+
+    const params: RequestInit = {
       method: 'POST',
       credentials: 'include',
-      headers: {
-        'Access-Control-Allow-Credentials': true,
-      },
-    })
-      .then(function (response) {
+      headers: headers,
+    };
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/api/logout`, params)
+      .then(response => {
         return response.json();
       })
-      .then(function (json) {
+      .then(json => {
         setIsAuthenticated(json.isAuthenticated);
       });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch(`${process.env.REACT_APP_SERVER_URL}/api/login`, {
+
+    const headers: HeadersInit = new Headers();
+    headers.set('Content-Type', 'application/json');
+    headers.set('Access-Control-Allow-Credentials', 'true');
+
+    const params: RequestInit = {
       method: 'POST',
       credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Credentials': true,
-      },
+      headers: headers,
       body: JSON.stringify({
         username: username,
         password: password,
       }),
-    })
-      .then(function (response) {
+    };
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/api/login`, params)
+      .then(response => {
         return response.json();
       })
-      .then(function (json) {
+      .then(json => {
         setIsAuthenticated(json.isAuthenticated);
       });
   };
@@ -106,12 +122,12 @@ function Header({ isAuthenticated, setIsAuthenticated }) {
     <nav>
       <ul>
         <li>
-          {isAuthenticated === true ? (
-            <button type="button" onClick={(e) => handleLogout(e)}>
+          {isAuthenticated ? (
+            <button type="button" onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleLogout(e)}>
               Logout
             </button>
           ) : (
-            <form onSubmit={(e) => handleSubmit(e)}>
+            <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
               <div>
                 <label>Username:</label>
                 <input
@@ -141,90 +157,97 @@ function Header({ isAuthenticated, setIsAuthenticated }) {
       </ul>
     </nav>
   );
-}
+};
 
-function Protected({ isAuthenticated, setIsAuthenticated }) {
+const Protected: React.FC<IAuthProps> = ({ isAuthenticated, setIsAuthenticated }) => {
   const [records, setRecords] = useState([]);
   const [summary, setSummary] = useState([]);
 
   const fetchTracks = () => {
-    fetch(`${process.env.REACT_APP_SERVER_URL}/api/tracks`, {
+    const headers: HeadersInit = new Headers();
+    headers.set('Access-Control-Allow-Credentials', 'true');
+
+    const params: RequestInit = {
       method: 'GET',
       credentials: 'include',
-      headers: {
-        'Access-Control-Allow-Credentials': true,
-      },
-    })
-      .then(function (response) {
+      headers: headers,
+    };
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/api/tracks`, params)
+      .then(response => {
         return response.json();
       })
-      .then(function (json) {
+      .then(json => {
         setRecords(json.tracks.reverse());
         setSummary(json.summary);
       });
   };
 
   useEffect(() => {
-    if (isAuthenticated === true) {
+    if (isAuthenticated) {
       fetchTracks();
     }
   }, [isAuthenticated]);
 
-  const handleAdd = (e, value) => {
+  const handleAdd = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, value: number) => {
     e.preventDefault();
+
     const newRecord = {
       counter: value,
     };
 
-    fetch(`${process.env.REACT_APP_SERVER_URL}/api/track`, {
+    const headers: HeadersInit = new Headers();
+    headers.set('Content-Type', 'application/json');
+    headers.set('Access-Control-Allow-Credentials', 'true');
+
+    const params: RequestInit = {
       method: 'POST',
       credentials: 'include',
-      headers: {
-        'Access-Control-Allow-Credentials': true,
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       body: JSON.stringify(newRecord),
-    })
-      .then(function (response) {
+    };
+
+    fetch(`${process.env.REACT_APP_SERVER_URL}/api/track`, params)
+      .then(response => {
         return response.json();
       })
-      .then(function (json) {
+      .then(() => {
         fetchTracks();
       });
   };
   return (
     <div>
-      {isAuthenticated === true ? (
+      {isAuthenticated ? (
         <div className="max-w-md mx-auto">
           <h1 className="text-5xl text-center">Cliff</h1>
           <div className="text-center">
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full h-16 w-16 m-3 border-solid border-4 border-green-600"
-              onClick={(e) => handleAdd(e, 0.2)}
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleAdd(e, 0.2)}
             >
               1/5
             </button>
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full h-16 w-16 m-3 border-solid border-4 border-green-600"
-              onClick={(e) => handleAdd(e, 0.25)}
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleAdd(e, 0.25)}
             >
               1/4
             </button>
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full h-16 w-16 m-3 border-solid border-4 border-green-600"
-              onClick={(e) => handleAdd(e, 0.33)}
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleAdd(e, 0.33)}
             >
               1/3
             </button>
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full h-16 w-16 m-3 border-solid border-4 border-green-600"
-              onClick={(e) => handleAdd(e, 0.5)}
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleAdd(e, 0.5)}
             >
               1/2
             </button>
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full h-16 w-16 m-3 border-solid border-4 border-green-600"
-              onClick={(e) => handleAdd(e, 1)}
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleAdd(e, 1)}
             >
               1
             </button>
@@ -234,7 +257,7 @@ function Protected({ isAuthenticated, setIsAuthenticated }) {
             <div className="border-b px-4 py-2 grid">Count</div>
             <div className="border-b px-4 py-2 grid col-span-3">Date</div>
           </div>
-          {summary.map((record, index) => {
+          {summary.map((record: any, index: number) => {
             return (
               <div className="grid grid-cols-4" key={index}>
                 <div className="border-b px-4 py-2 grid">{record.counter}</div>
@@ -249,7 +272,7 @@ function Protected({ isAuthenticated, setIsAuthenticated }) {
             <div className="border-b px-4 py-2 grid">Count</div>
             <div className="border-b px-4 py-2 grid col-span-3">Date</div>
           </div>
-          {records.map((record, index) => {
+          {records.map((record: any, index) => {
             return (
               <div className="grid grid-cols-4" key={index}>
                 <div className="border-b px-4 py-2 grid">{record.counter}</div>
@@ -267,4 +290,4 @@ function Protected({ isAuthenticated, setIsAuthenticated }) {
       )}
     </div>
   );
-}
+};
